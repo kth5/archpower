24b8f4dc2bdc817fc9e91c4970bebf634268841f
Alexander Baldeck <alex.bldck@gmail.com>
2021-05-25 11:57:20 +0200

    * update perl to 5.34.0-1

--- perl/perl/generate-rebuild-list.sh
+++ perl/perl/generate-rebuild-list.sh
@@ -6,8 +6,9 @@ echo "vim"
 
 pkgfile -rd "^/usr/lib/perl5/" | sed 's#^.*/##' | sort -u
 
+ssh build.archlinux.org sogrep -r
 for repo in core extra community multilib; do
-	ssh dragon.archlinux.org sogrep "$repo" libperl.so
+	ssh build.archlinux.org sogrep "$repo" libperl.so
 done
 
 # this one is optional
