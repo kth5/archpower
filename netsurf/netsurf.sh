fd10c8bee04073109a7774966ce5717a73bee3bf
kth5 <alex.bldck@gmail.com>
2024-02-16 09:10:21 +0100

    * update netsurf to 3.11-4

--- netsurf/netsurf.sh
+++ netsurf/netsurf.sh
@@ -1,4 +1,7 @@
-#!/bin/bash
-# Use English if a directory for the current language does not exist
-test -d "/usr/share/netsurf/${LANG:0:2}" || export LANG=en_US.UTF-8
+#!/bin/sh
+LANG_PREFIX=$(expr substr "$LANG" 1 2)
+if [ ! -d "/usr/share/netsurf/$LANG_PREFIX" ]; then
+  # Use English if a directory for the current language does not exist
+  export LANG=en_US.UTF-8
+fi
 /usr/bin/netsurf-gtk3 "$@"
