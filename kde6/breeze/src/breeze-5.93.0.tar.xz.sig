28df3815ca9a8b069b1607e708ef17e0ecf77dd2
kth5 <alex.bldck@gmail.com>
2024-02-19 00:02:48 +0100

    * disable bidi renderer causing crashes on bigendian powerpc

--- kde6/breeze/src/breeze-5.93.0.tar.xz.sig
+++
@@ -1,1 +0,0 @@
-/mnt/caches/sources/breeze-5.93.0.tar.xz.sig
--- kde6/breeze/src/breeze-5.93.0/kdecoration/config/kcm_breezedecoration.cpp
+++
@@ -1,6 +0,0 @@
-#include "breezeconfigwidget.h"
-#include <KPluginFactory>
-
-K_PLUGIN_CLASS_WITH_JSON(Breeze::ConfigWidget, "kcm_breezedecoration.json")
-
-#include "kcm_breezedecoration.moc"
