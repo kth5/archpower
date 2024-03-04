1071ab89cc9dc265b91889c9167e3c17beb84335
kth5 <alex.bldck@gmail.com>
2024-03-04 15:44:38 +0100

    * upgrade Plasma to 6.0.0 & Gear to 24.02

--- kde/akonadi-calendar/akonadi-calendar.orig/src/akonadi-calendar-24.01.95/serializers/Messages.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-$XGETTEXT *.cpp -o $podir/libakonadi-calendar6-serializer.pot
-
--- kde/akonadi-calendar/akonadi-calendar.orig/src/akonadi-calendar-24.01.95/src/Messages.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/sh
-# SPDX-License-Identifier: CC0-1.0
-# SPDX-FileCopyrightText: none
-$EXTRACTRC `find . -name \*.ui -o -name \*.kcfg` >> rc.cpp || exit 11
-$XGETTEXT `find . -name "*.cpp" -o -name "*.h"` -o $podir/libakonadi-calendar6.pot
-rm -f rc.cpp
