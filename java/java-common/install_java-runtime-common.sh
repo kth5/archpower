c2c09cafd4a02cfdc1399d437cccf740e5203f3c
kth5 <alex.bldck@gmail.com>
2025-05-19 11:31:31 +0200

    * update java-common to 3-6

--- java/java-common/install_java-runtime-common.sh
+++ java/java-common/install_java-runtime-common.sh
@@ -7,7 +7,7 @@ pre_upgrade() {
 
 post_install() {
   echo 'For the complete set of Java binaries to be available in your PATH,'
-  echo 'you need to re-login or source /etc/profile.d/jre.sh'
+  echo 'you need to re-login or source /etc/profile'
   echo 'Please note that this package does not support forcing JAVA_HOME as former package 'java-common' did'
 }
 
