8eecbc36227e9c53eac0cfc7ac9862037980c735
kth5 <alex.bldck@gmail.com>
2025-09-03 12:02:20 +0200

    * update pyenv to 1:2.6.7-1

---
+++ python/pyenv/pyenv.zsh
@@ -0,0 +1,28 @@
+#compdef pyenv
+if [[ ! -o interactive ]]; then
+    return
+fi
+
+local state line
+typeset -A opt_args
+
+_arguments -C \
+    {--help,-h}'[Show help]' \
+    {--version,-v}'[Show pyenv version]' \
+    '(-): :->command' \
+    '*:: :->option-or-argument'
+
+case "$state" in
+    (command)
+        local -a commands
+        commands=(${(f)"$(pyenv commands)"})
+        _describe -t commands 'command' commands
+        ;;
+    (option-or-argument)
+        local -a args
+        args=(${(f)"$(pyenv completions ${line[1]})"})
+        _describe -t args 'arg' args
+        ;;
+esac
+
+return
